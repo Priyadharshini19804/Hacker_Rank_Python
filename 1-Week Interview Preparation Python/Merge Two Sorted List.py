df84cbfb3b107b92599267ca40608acf0359f9a6
Priyadharshini S M <167222422+Priyadharshini19804@users.noreply.github.com>
2025-07-16 20:42:22 +0530

    Merge Two Sorted List.py

---
+++ 1-Week Interview Preparation Python/Merge Two Sorted List.py
@@ -0,0 +1,110 @@
+"""
+
+Given pointers to the heads of two sorted linked lists, merge them into a single, sorted linked list. 
+Either head pointer may be null meaning that the corresponding list is empty.
+
+Sample Input
+
+1
+3
+1
+2
+3
+2
+3
+4
+
+Sample Output
+
+1 2 3 3 4 
+
+"""
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def mergeLists(head1, head2):
+    # Create a dummy node to start the merged list
+    dummy = SinglyLinkedListNode(0)
+    tail = dummy
+
+    # Traverse both lists and append the smaller node
+    while head1 and head2:
+        if head1.data < head2.data:
+            tail.next = head1
+            head1 = head1.next
+        else:
+            tail.next = head2
+            head2 = head2.next
+        tail = tail.next
+
+    # Attach the remaining part (if any)
+    if head1:
+        tail.next = head1
+    if head2:
+        tail.next = head2
+
+    return dummy.next
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist1_count = int(input())
+
+        llist1 = SinglyLinkedList()
+
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+            
+        llist2_count = int(input())
+
+        llist2 = SinglyLinkedList()
+
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
+
+        llist3 = mergeLists(llist1.head, llist2.head)
+
+        print_singly_linked_list(llist3, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
